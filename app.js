a07dd410990e9de31135a905f069356ed08f2879
JohnnyLima22 <johnnyemanoel22@gmail.com>
2025-08-03 20:33:27 -0300

    final

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Lista onde os nomes serão armazenados
+// Lista onde os nomes
 let listaDeAmigos = [];
 
 // Coloca o título da página
@@ -9,22 +9,41 @@ titulo.innerHTML = 'Amigo Secreto';
 let titulo2 = document.querySelector('h2');
 titulo2.innerHTML = 'Digite o nome dos seus amigos';
 
-// Função clicar no "Adicionar" .trim usando para tirar espaços. 
+// Função chamada ao clicar no "Adicionar" , o trim remove espaços desnecessários
 function adicionarAmigo() {
     let input = document.getElementById("amigo");
-    let nome = input.value.trim();
-    
+    let nome = input.value.trim(); 
+
     if (nome === '') {
         alert("Por favor, digite um nome.");
         return;
     }
-
+// ver se o nome ja foi escrito
     if (listaDeAmigos.includes(nome)) {
         alert("Esse nome já foi adicionado.");
         input.value = '';
         return;
     }
-    
+
     // Adiciona o nome na lista
     listaDeAmigos.push(nome);
+
+    // Mostra a lista na tela
+    atualizarLista();
+
+    // Limpa o campo
+    input.value = '';
+    
+}
+
+// Atualiza o conteúdo da <ul id="listaAmigos">
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = '';
+
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = listaDeAmigos[i];
+        lista.appendChild(li);
+    }
 }
