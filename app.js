33b9cd12a017de491ff1381447930c98b027ce91
JohnnyLima22 <johnnyemanoel22@gmail.com>
2025-08-03 20:08:52 -0300

    jogo_do_amigo_secreto

---
+++ app.js
@@ -0,0 +1,11 @@
+// Lista onde os nomes serão armazenados
+let listaDeAmigos = [];
+
+// Coloca o título da página
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Amigo Secreto';
+
+// Coloca o subtítulo
+let titulo2 = document.querySelector('h2');
+titulo2.innerHTML = 'Digite o nome dos seus amigos';
+
